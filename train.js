909e1d0ab18af520b9e8e9d3160ebcfe6c0caba6
ehab yasser <ehabyasser82@gmail.com>
2025-05-03 17:44:28 +0300

    Fix checked toggle

--- train.js
+++ train.js
@@ -5,9 +5,11 @@ const btn2 = document.getElementById("btn2")
 const btnDiv = document.getElementById("btnCont")
 const popup = document.getElementsByClassName("pop")[0];
 
+let isAnimated = false;
+
 button.addEventListener("click", myFunction)
 function myFunction() {
-  let output = input.value
+  let output = input.value.trim();
   if (output === "") {
     popup.classList.remove("animation2");
     popup.classList.remove("animation")
@@ -15,7 +17,7 @@ function myFunction() {
     setTimeout(() => {
       popup.classList.remove("animation2");
       popup.classList.add("animation")
-    }, 2000);
+    }, 2500);
   } else {
     input.value = ""
     // create li
@@ -46,8 +48,9 @@ function myFunction() {
 
     // make it checked
     create.addEventListener("click", function () {
-      para.classList.toggle("checked")
+      create.classList.toggle("checked")
     })
+    
     //btn2
     btnDiv.style.display = "flex"
   }
